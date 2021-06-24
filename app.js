57742647a5b687e0b31d26d51582d6e9212d99f5
Yi donghoon <icq4ever@gmail.com>
2021-06-24 17:15:21 +0900

    commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const onvif = require('node-onvif');
+
+// Create an OnvifDevice object
+let device = new onvif.OnvifDevice({
+	xaddr: 'http://192.168.0.101:8080/onvif/device_service',
+	user : 'USERNAME',
+	pass : 'PASSWD'
+});
+
+// Initialize the OnvifDevice object
+device.init().then(() => {
+	// Move the camera
+	return device.ptzMove({
+		'speed': {
+			z: 1.0  // Speed of zoom (in the range of -1.0 to 1.0)
+		},
+		'timeout': 1 // seconds
+	});
+}).then(() => {
+	console.log('Done!');
+}).catch((error) => {
+	console.error(error);
+});
